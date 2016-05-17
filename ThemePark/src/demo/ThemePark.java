262f7651badb041f86956eeff7bda9f18fc3433b
Student5 <Student5@10.8.33.139>
2016-05-17 14:00:49 -0400

    nsrljhnslkrj

--- ThemePark/src/demo/ThemePark.java
+++ ThemePark/src/demo/ThemePark.java
@@ -2,20 +2,21 @@ package demo;
 import java.util.ArrayList;
 
 public class ThemePark {
-	private static int numPeople;
-	private static int numOfDest;
-	private static int priceCoef;	
-	private static int numOfLavatories;
-	private static int numOfRides;
-	private static int numOfKiosks;
-	private static int numOfVendors;
+	private int numPeople;
+	private int numOfDest;
+	private int priceCoef;	
+	private int numOfLavatories;
+	private int numOfRides;
+	private int numOfKiosks;
+	private int numOfVendors;
 	ArrayList<Person> peopleInPark;
 	ArrayList<Destination> allDestinations = new ArrayList<Destination>();
 	//Array list of shops/vendors/rides (activities)
 	
-	public ThemePark(int nop){
+	public ThemePark(int numPeople, int priceCoef){
 		peopleInPark = new ArrayList<Person>();
-		numPeople = peopleInPark.size();
+		this.numPeople = numPeople;
+		this.priceCoef = priceCoef;
 		//this.activity = activity;
 		//numRides = num of activities in activity with type ride
 		//numShops = num of activities in activity with type shop
@@ -32,31 +33,68 @@ public class ThemePark {
 	 * ARIKS SHIT
 	 * 
 	 */
-	public static void generateNumberDestinations(){
-		double numOfDestinations = getNumPeople()/10;
-		numOfLavatories = (int) (numOfDestinations*.2);
-		numOfRides = (int)(numOfDestinations*.4);
-		numOfKiosks = (int)(numOfDestinations*.2);
-		numOfVendors = (int)(numOfDestinations*.2);
-	}
 
-	/*
-	 * 
-	 * 
-	 * GETTER / SETTER
-	 */
-	public static int getNumPeople() {
+	public int getNumPeople() {
 		return numPeople;
 	}
 
-	public static int getNumOfDest() {
+	public void setNumPeople(int numPeople) {
+		this.numPeople = numPeople;
+	}
+
+	public int getNumOfDest() {
 		return numOfDest;
 	}
 
-	public static int getPriceCoef() {
+	public void setNumOfDest(int numOfDest) {
+		this.numOfDest = numOfDest;
+	}
+
+	public int getPriceCoef() {
 		return priceCoef;
 	}
-	public static int getNumOfDestinations(){
-		return numOfLavatories + numOfRides + numOfKiosks + numOfVendors;
+
+	public void setPriceCoef(int priceCoef) {
+		this.priceCoef = priceCoef;
 	}
+
+	public int getNumOfLavatories() {
+		return numOfLavatories;
+	}
+
+	public void setNumOfLavatories(int numOfLavatories) {
+		this.numOfLavatories = numOfLavatories;
+	}
+
+	public int getNumOfRides() {
+		return numOfRides;
+	}
+
+	public void setNumOfRides(int numOfRides) {
+		this.numOfRides = numOfRides;
+	}
+
+	public int getNumOfKiosks() {
+		return numOfKiosks;
+	}
+
+	public void setNumOfKiosks(int numOfKiosks) {
+		this.numOfKiosks = numOfKiosks;
+	}
+
+	public int getNumOfVendors() {
+		return numOfVendors;
+	}
+
+	public void setNumOfVendors(int numOfVendors) {
+		this.numOfVendors = numOfVendors;
+	}
+
+	public ArrayList<Person> getPeopleInPark() {
+		return peopleInPark;
+	}
+
+	public void setPeopleInPark(ArrayList<Person> peopleInPark) {
+		this.peopleInPark = peopleInPark;
+	}	
 }

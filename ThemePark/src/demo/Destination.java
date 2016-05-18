b7b1fb42741db643b68d7829bc2c49046b5582f2
Student8 <Student8@10.8.33.147>
2016-05-18 13:25:22 -0400

    Fixed small errors in dest

--- ThemePark/src/demo/Destination.java
+++ ThemePark/src/demo/Destination.java
@@ -7,14 +7,14 @@ public class Destination {
 	private int cost;
 	private boolean isOpen;
 	private ArrayList<Person> peopleAttending;
-	private int type;
+	//private int type;
 	
 	public Destination(int maxPoints,int  cost, boolean openStatus) {
 		this.maxPoints = maxPoints;
 		this.cost = cost;
 		this.isOpen = openStatus;
 		this.peopleAttending = new ArrayList<Person>();
-		this.type= type;
+		//this.type= type;
 	}
 
 	public int getMaxPoints() {
@@ -49,12 +49,5 @@ public class Destination {
 		this.peopleAttending = peopleAttending;
 	}
 
-	public int getType() {
-		return type;
-	}
-
-	public void setType(int type) {
-		this.type = type;
-	}
 }
 
--- ThemePark/src/demo/Kiosk.java
+++ ThemePark/src/demo/Kiosk.java
@@ -4,7 +4,7 @@ public class Kiosk extends Destination {
 	private int refilAmount;
 	public Kiosk(int maxPoints,int  cost, boolean openStatus, int rAmount) {
 		// TODO Auto-generated constructor stub
-		super(maxPoints,cost,openStatus,type);
+		super(maxPoints,cost,openStatus);
 		refilAmount = rAmount;
 	}
 

d84b61721c83ebbf3e06d1e8192266f15a90e287
Student5 <Student5@10.8.33.139>
2016-05-18 13:52:58 -0400

    Changed person and added a new interface checkPerson

--- ThemePark/src/demo/Person.java
+++ ThemePark/src/demo/Person.java
@@ -1,23 +1,34 @@
 package demo;
 
+import java.util.ArrayList;
+
 public class Person implements PersonInterface {
 	private int points;
 	private int height;
 	private boolean isAdult;
+	private int excitement;
+	private int needToDefecate;
+	private int fatigue;
 	private Destination currLocation;
 	
-	public Person(int points, int height, boolean isAdult, Destination currLocation){
+	public Person(int points, int height, boolean isAdult, Destination currLocation, int excitement, int needToDefecate, int fatigue){
 		this.points = points;
 		this.height = height;
 		this.isAdult = isAdult;
+		this.excitement = excitement;
+		this.needToDefecate = needToDefecate;
+		this.fatigue = fatigue;
 		this.currLocation = currLocation;
 	}
 	
 	public void doesActivity(Destination dest) {
+		//If the destination is open must be checked before caling this method
 		this.currLocation = dest;
 		dest.setMaxPoints(dest.getMaxPoints() - dest.getCost());
 		this.points -= dest.getCost();
-		
+		ArrayList<Person> peopleAtDest = dest.getPeopleAttending();
+		peopleAtDest.add(this);
+		dest.setPeopleAttending(peopleAtDest);
 	}
 	
 	public int getPoints() {
---
+++ ThemePark/src/demo/checkPerson.java
@@ -0,0 +1,6 @@
+package demo;
+
+public interface checkPerson {
+	boolean isAboveHeight(int rideMinHeight);
+	boolean canPay(int destCost);	
+}
